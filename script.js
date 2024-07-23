9d7bc72a06f39146a2a32e356c9727ec65788728
Haruki <carlosanchezvilar2002@gmail.com>
2024-07-23 03:46:11 +0200

    Hover & Navigation
    - Al hacer hover en los personajes ahora hay una pequeña animación que agranda la ficha
    - Ahora hay un nuevo boton el cual te lleva a una semana seleccionada

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
         var text = heading.innerText; // Almacenar el texto original
         var newHtml = ''; // Variable para mantener el nuevo HTML
 
-        if (text.length > 0) { // Verifica que el texto no esté vacío
-            // Bucle a través de cada carácter en el texto original
+        if (text.length > 0) { // Verifica que el texto no estï¿½ vacï¿½o
+            // Bucle a travï¿½s de cada carï¿½cter en el texto original
             for (var i = 0; i < text.length; i++) {
                 var color = Math.random() > 0.5 ? 'black' : 'white'; // Elige aleatoriamente negro o blanco
-                newHtml += '<span style="color:' + color + ';">' + text[i] + '</span>'; // Envuelve cada carácter en un span con estilo
+                newHtml += '<span style="color:' + color + ';">' + text[i] + '</span>'; // Envuelve cada carï¿½cter en un span con estilo
             }
 
             heading.innerHTML = newHtml; // Reemplaza el texto original con el nuevo HTML
         } else {
-            console.log("El texto está vacío");
+            console.log("El texto estï¿½ vacï¿½o");
         }
     } else {
         console.log("Elemento no encontrado en el documento");
